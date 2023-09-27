5fb445fc234d654aa3c41de62c7c5a7ead274fc9
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-09-27 21:25:25 +0100

    move grid changing logic to grid module

---
+++ src/grid.js
@@ -0,0 +1,32 @@
+export function createGrid(userChoice, drawingPad) {
+  // Clear the board
+  while (drawingPad.firstChild) {
+    drawingPad.removeChild(drawingPad.firstChild);
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  drawingPad.style.gridTemplateColumns = `repeat(${userChoice}, 1fr)`;
+  // eslint-disable-next-line no-param-reassign
+  drawingPad.style.gridTemplateRows = `repeat(${userChoice}, 1fr)`;
+
+  for (let i = 0; i < userChoice ** 2; i += 1) {
+    const cell = document.createElement('div');
+    cell.className = 'cells';
+    cell.style.border = 'solid black 1px';
+    drawingPad.appendChild(cell);
+  }
+}
+
+export function setSize() {
+  const userInput = prompt(
+    'Give your desired size of drawing grid, only integer values from 16-100 are valid',
+  );
+  const userChoice = parseInt(userInput, 10);
+  if (userChoice <= 16 && userChoice >= 100) {
+    alert('Please choose a value between 16-100.');
+    return null;
+  }
+  const drawingPad = document.querySelector('.drawingPad'); // replace '.drawingPad' with the correct selector for your drawing pad
+  createGrid(userChoice, drawingPad);
+  return userChoice;
+}
